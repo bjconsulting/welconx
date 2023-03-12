9c05d7ee239b2686e9a9bff85f1299746d11961e
Bruno Oliveira Julião <2057387+brunojuliao@users.noreply.github.com>
2023-03-12 19:41:50 -0300

    Dobra 9 (#2)

---
+++ conx/src/Dobra9.tsx
@@ -0,0 +1,10 @@
+function Dobra9(){
+    return(
+        <div className="w-full mt-32">
+          <p className="text-xl text-center my-16">Rua Eugênio de Medeiros, 345</p>
+          <iframe className="w-full aspect-[3.09] border-none" title="Local no Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.0056275488428!2d-46.70262668445346!3d-23.568241367732814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce57a88c188e0d%3A0xa089f0469a931d01!2sR.%20Eug%C3%AAnio%20de%20Medeiros%2C%20345%20-%20Pinheiros%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005425-000!5e0!3m2!1sen!2sbr!4v1678659529282!5m2!1sen!2sbr" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    )
+}
+
+export default Dobra9;
