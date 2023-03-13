0092d187cbfb04b2d99f3898c485eea6cb53f6bc
Bruno Juliao <2057387+brunojuliao@users.noreply.github.com>
2023-03-13 15:18:13 -0300

    Ajustando footer e disposição de plantas

--- conx/src/Dobra10.tsx
+++ conx/src/Dobra10.tsx
@@ -4,10 +4,10 @@ import conxLogo from './img/conx-logo.png'
 
 export default function Dobra10() {
   return(
-    <div className='w-full flex flex-row items-start text-center justify-evenly pt-20 pb-20 font-semibold text-base'>
+    <div className='w-full flex flex-col items-center text-center gap-8 justify-center pt-20 pb-20 font-semibold text-base tablet:flex-row tablet:items-start tablet:text-center :justify-evenly'>
       <div className='flex flex-col gap-8'>
         <div className='uppercase'>Intermediação:</div>
-        <div className='flex flex-row gap-9'>
+        <div className='flex flex-col gap-9 tablet:flex-row'>
           <img src={conxVendas} alt="Logo da empresa Conx Vendas"/>
           <img src={houzi} alt="Logo da empresa Houzi Soluções Imobiliárias"/>
         </div>
