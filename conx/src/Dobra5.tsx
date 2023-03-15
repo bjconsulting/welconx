beb21325b989a3efed4b4849663a31489f7edf10
Bruno Juliao <2057387+brunojuliao@users.noreply.github.com>
2023-03-15 15:58:40 -0300

    Carrossel 1 funcionando

--- conx/src/Dobra5.tsx
+++ conx/src/Dobra5.tsx
@@ -6,13 +6,46 @@ import { GiWashingMachine } from "react-icons/gi";
 import { RiStoreLine } from "react-icons/ri";
 import { FiPackage } from "react-icons/fi";
 import { BsBicycle } from "react-icons/bs";
-// import { GrNext } from "react-icons/gr";
-// import { GrPrevious } from "react-icons/gr";
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 import NextImgSrc from './NextImgSrc';
-// import img2 from '../public/img/lavanderia.jpg'
 
 function Dobra5() {
-    return(
+
+    const panel = React.useRef<HTMLDivElement>(null)
+
+    const prev = () => {
+        if (!panel.current) return
+
+        const container = panel.current
+        const items = container.querySelectorAll('.snap-start') as NodeListOf<HTMLDivElement>
+        const containerRect = container.getBoundingClientRect()
+        const currentItemIndex = Math.round(container.scrollLeft / containerRect.width)
+
+        if (currentItemIndex > 0) {
+            const prevItem = items[currentItemIndex - 1]
+            const prevItemRect = prevItem.getBoundingClientRect()
+            container.scrollBy({ left: prevItemRect.left - containerRect.left, behavior: 'smooth' })
+        }
+    }
+
+    const next = () => {
+        if (!panel.current) return
+
+        const container = panel.current
+        const items = container.querySelectorAll('.snap-start') as NodeListOf<HTMLDivElement>
+        const containerRect = container.getBoundingClientRect()
+        const currentItemIndex = Math.round(container.scrollLeft / containerRect.width)
+
+        if (currentItemIndex < items.length - 1) {
+            const nextItem = items[currentItemIndex + 1]
+            const nextItemRect = nextItem.getBoundingClientRect()
+            container.scrollBy({ left: nextItemRect.left - containerRect.left, behavior: 'smooth' })
+            console.log({container, scrollLeft:container.scrollLeft, left: nextItemRect.left - containerRect.left, behavior: 'smooth' })
+        }
+    }
+
+    return (
         <div className='bg-[rgba(243,_232,_225,_.64)] w-screen'>
             <div className='m-r flex flex-col mx-auto py-[2em] gap-12'>
                 <h2 className='text-[#243548] text-[1.4em] laptop:text-[2.4em] text-center font-bold max-w-[800px] mx-auto mb-9'>
@@ -21,22 +54,49 @@ function Dobra5() {
 
                 <div className='flex flex-col laptop:flex-row gap-20 mx-auto justify-center'>
                     <div className='left-container relative h-auto'>
-                        <div className='relative image-container'>
-                            <picture>
-                                <source srcSet={NextImgSrc("img/market.avif")} type='image/avif'/>
-                                <source srcSet={NextImgSrc("img/market.webp")} type='image/webp'/>
-                                <img className=' aspect-auto max-w-[250px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px] mx-auto' src={NextImgSrc("img/market.jpg")} alt="Mini Market" loading='lazy' />
-                            </picture>
-                            <p className='absolute right-4 bottom-4 uppercase text-[.4em] text-white'> perspectiva ilustrada do Mini Market</p>
-                            {/* <div className='p-2 tablet:p-4 rounded-full bg-[#FEFEFE] opacity-80 absolute top-[45%] right-8 hover:opacity-100 cursor-pointer'>
-                                <GrNext className='fill-prim'/>
+                        <div ref={panel} className='relative image-container hide-scroll aspect-[1.333333333333333] overflow-x-scroll scroll-smooth snap-x snap-mandatory snap-always max-w-[326px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px]'>
+                            <div className='flex flex-row'>
+                                <div className='relative snap-start'>
+                                    <picture>
+                                        <source srcSet={NextImgSrc("img/lavanderia.avif")} type='image/avif' />
+                                        <source srcSet={NextImgSrc("img/lavanderia.webp")} type='image/webp' />
+                                        <img className=' aspect-[1.47] w-[700px] max-w-[326px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px] mx-auto' src={NextImgSrc("img/lavanderia.jpg")} alt="Lavanderia" loading='lazy' />
+                                    </picture>
+                                    <p className='absolute right-4 bottom-4 uppercase text-[.4em] text-white'> perspectiva ilustrada da Lavanderia</p>
+                                </div>
+                                <div className='relative snap-start'>
+                                    <picture>
+                                        <source srcSet={NextImgSrc("img/market.avif")} type='image/avif' />
+                                        <source srcSet={NextImgSrc("img/market.webp")} type='image/webp' />
+                                        <img className=' aspect-[1.47] w-[700px] max-w-[326px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px] mx-auto' src={NextImgSrc("img/market.jpg")} alt="Mini Market" loading='lazy' />
+                                    </picture>
+                                    <p className='absolute right-4 bottom-4 uppercase text-[.4em] text-white'> perspectiva ilustrada do Mini Market</p>
+                                </div>
+                                <div className='relative snap-start'>
+                                    <picture>
+                                        <source srcSet={NextImgSrc("img/delivery.avif")} type='image/avif' />
+                                        <source srcSet={NextImgSrc("img/delivery.webp")} type='image/webp' />
+                                        <img className=' aspect-[1.47] w-[700px] max-w-[326px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px] mx-auto' src={NextImgSrc("img/delivery.jpg")} alt="Espaço Delivery" loading='lazy' />
+                                    </picture>
+                                    <p className='absolute right-4 bottom-4 uppercase text-[.4em] text-white'> perspectiva ilustrada do Espaço Delivery</p>
+                                </div>
+                                <div className='relative snap-start'>
+                                    <picture>
+                                        <source srcSet={NextImgSrc("img/bike.avif")} type='image/avif' />
+                                        <source srcSet={NextImgSrc("img/bike.webp")} type='image/webp' />
+                                        <img className=' aspect-[1.47] w-[700px] max-w-[326px] laptop:max-w-[300px] desktop:max-w-[500px] desktop-big:max-w-[700px] mx-auto' src={NextImgSrc("img/bike.jpg")} alt="Bike Elétrica Compartilhada" loading='lazy' />
+                                    </picture>
+                                    <p className='absolute right-4 bottom-4 uppercase text-[.4em] text-white'> perspectiva ilustrada de Bike Elétrica Compartilhada</p>
+                                </div>
                             </div>
-                            
-                            <div className='p-2 tablet:p-4 rounded-full bg-[#FEFEFE] opacity-80 absolute top-[45%] left-8 hover:opacity-100 cursor-pointer'>
-                                <GrPrevious className='fill-prim'/>
-                            </div> */}
+                        </div>
+                        <div className='p-2 tablet:p-4 rounded-full bg-[#FEFEFE] opacity-80 absolute -mt-[50%] translate-y-1/2 right-8 hover:opacity-100 cursor-pointer' onClick={next}>
+                            <GrNext className='fill-prim' />
                         </div>
 
+                        <div className='p-2 tablet:p-4 rounded-full bg-[#FEFEFE] opacity-80 absolute -mt-[50%] translate-y-1/2 left-8 hover:opacity-100 cursor-pointer' onClick={prev}>
+                            <GrPrevious className='fill-prim' />
+                        </div>
 
                         <img src={NextImgSrc("img/well-services.png")} alt="Well Services" loading='lazy' className='max-w-[300px] mt-6 mx-auto' />
                     </div>
@@ -44,7 +104,7 @@ function Dobra5() {
                     <div className="basis-[55%] flex flex-col gap-6">
                         <div className="flex flex-col tablet:flex-row tablet:justify-start items-center gap-4 border-e">
                             <div className='p-6 rounded-full bg-prim'>
-                                <GiWashingMachine className='w-8 h-8 fill-sub'/>
+                                <GiWashingMachine className='w-8 h-8 fill-sub' />
                             </div>
 
                             <div className='flex flex-col gap-2'>
