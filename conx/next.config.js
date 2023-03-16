c21c8c730b2d8f8040a7a9b1eb47fd0ca79e51bb
Bruno Juliao <2057387+brunojuliao@users.noreply.github.com>
2023-03-15 21:11:59 -0300

    Apontando para prod

--- conx/next.config.js
+++ conx/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  basePath: '/welconxpinheiros-teste',
+  basePath: '/welconxpinheiros',
+  //basePath: '/welconxpinheiros-teste',
   images: {
     unoptimized: true
   }
--- conx/src/NextImgSrc.tsx
+++ conx/src/NextImgSrc.tsx
@@ -1,6 +1,6 @@
 export default function NextImgSrc(relativePath: string) {
   //const base = '';
-  //const base = '/welconxpinheiros';
-  const base = '/welconxpinheiros-teste';
+  const base = '/welconxpinheiros';
+  //const base = '/welconxpinheiros-teste';
   return `${base}/${relativePath}`;
 }
