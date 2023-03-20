70b9667d7dc65db8fcf82722d1c98cd46ab1b980
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-03-20 17:40:27 -0300

    adicionando iframe

--- conx/src/Dobra3.tsx
+++ conx/src/Dobra3.tsx
@@ -8,11 +8,13 @@ function Dobra3(){
             {/* <div className=' min-h-[30vh] laptop:min-h-[90vh] w-screen bg-bg-3  bg-[length:auto_100%]  laptop:bg-[length:100%_100%]'>
 
             </div> */}
-            <picture>
+            {/* <picture>
                 <source srcSet={NextImgSrc("img/bg-3.avif")} type='image/avif'/>
                 <source srcSet={NextImgSrc("img/bg-3.webp")} type='image/webp'/>
                 <img src={NextImgSrc("img/bg-3.jpg")} alt="" className='h-auto w-auto max-w-[100%] tablet:max-w-[100vw] mx-auto 4k:w-[100svw]' width="100" height="100" loading='lazy' />
-            </picture>
+            </picture> */}
+            <iframe src="https://yourside.com.br/jobs/Conx/Pinheiros/Planet/Welconx_Pinheiros.html" frameBorder="0"
+                className='w-full aspect-[16/9] laptop:aspect-[16/5]'></iframe>
         </>
     )
 }
