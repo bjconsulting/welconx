3486d859d5e28db62b50c4c7dd1a521e4e84c9d0
Bruno Juliao <2057387+brunojuliao@users.noreply.github.com>
2023-03-24 17:51:23 -0300

    Corrigindo o title

--- conx/pages/_app.tsx
+++ conx/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>CONX - Imóveis Pinheiros</title>
+        <title>Welconx Pinheiros</title>
       </Head>
       <Component {...pageProps} />
     </>
