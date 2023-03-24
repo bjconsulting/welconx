6294cacef4ca7651b0489392580d96a483db3d72
Bruno Juliao <2057387+brunojuliao@users.noreply.github.com>
2023-03-24 15:10:20 -0300

    Acerto na tag HEAD para funcionar os META

--- conx/pages/_app.tsx
+++ conx/pages/_app.tsx
@@ -6,6 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>CONX - Imóveis Pinheiros</title>
       </Head>
       <Component {...pageProps} />
--- conx/pages/_document.tsx
+++ conx/pages/_document.tsx
@@ -5,6 +5,29 @@ export default function Document() {
   return (
     <Html lang='pt-BR'>
       <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Welconx - Pinheiros - O urbano que te envolve em um dos bairros mais desejados de São Paulo"
+        />
+        <meta property="image" content="fachada.jpg"/>
+        <link rel="apple-touch-icon" href="logo192.png" />
+        <link rel="manifest" href="manifest.json" />
+
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{
+          __html:
+          `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MKR3L52');
+          `
+        }}/>
+        {/* <!-- End Google Tag Manager --> */}
         <meta
           name="description"
           content="Welconx - Pinheiros - O urbano que te envolve em um dos bairros mais desejados de São Paulo"
@@ -21,6 +44,12 @@ export default function Document() {
         <meta property="og:locale" content="pt_BR" />
       </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKR3L52"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}/>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
